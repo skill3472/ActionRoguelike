8824b049d3c1f92eedb45aacabd4f03d2db7d1c5
Maksymilian Tym <tymszczur@gmail.com>
2024-04-04 19:36:55 +0200

    Lecture 10
    AI :+1:

---
+++ Source/ActionRoguelike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AI/SBTService_CheckAttackRange.h"
+
+#include "AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	UBlackboardComponent* bbc = OwnerComp.GetBlackboardComponent();
+	if(ensure(bbc))
+	{
+		AActor* TargetActor = Cast<AActor>(bbc->GetValueAsObject("TargetActor"));
+		if(TargetActor)
+		{
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if(ensure(MyController))
+			{
+				APawn* AIPawn = MyController->GetPawn();
+				if(ensure(AIPawn))
+				{
+					float DistanceTo = FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+
+					bool bWithinRange = DistanceTo < 2000.0f;
+					bool bHasLineOfSight = false;
+					if(bWithinRange)
+					{
+						bHasLineOfSight = MyController->LineOfSightTo(TargetActor);
+					}
+					
+					bbc->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinRange && bHasLineOfSight));
+				}
+			}
+		}
+	}
+}
